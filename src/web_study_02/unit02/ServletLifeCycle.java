806fdc6879b766babd1607988289c9836d58c4c2
seungeun <tmddms0931@naver.com>
2020-08-18 11:03:08 +0900

    수정..

--- src/web_study_02/unit02/ServletLifeCycle.java
+++ src/web_study_02/unit02/ServletLifeCycle.java
@@ -31,6 +31,7 @@ public class ServletLifeCycle extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doPost()");
 		doGet(request, response);
 	}
 
